03adb8b619e3c8df2ec040a0c4287e92732561a1
scotchmint <nullptr91@tuta.io>
2022-12-13 13:22:30 +0200

    Add buttons for: choose color, erase, toggle lines, reset, change grid size, rgb

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function randomNumber()
 {
     let random = Math.floor(Math.random() * 100);
 
-    return random;
+    if (random > 1 && random <= 100)
+    {
+        return random;
+    }
+    else
+    {
+        return 16;
+    }
+}
+
+function resetGrid(numOfColumns)
+{
+    for(let i = 0; i < numOfColumns; ++i)
+    {
+        grid.removeChild(grid.firstChild);
+    }
 }
 
 let grid = document.querySelector('.grid');
@@ -11,6 +26,14 @@ let gridSize = document.querySelector('.btn-size');
 
 let numOfColumns = 16;
 makeGrid(numOfColumns);
+
+gridSize.addEventListener('click', () => {
+
+    resetGrid(numOfColumns);
+
+    numOfColumns = randomNumber();
+    makeGrid(numOfColumns);
+});
 
 function makeGrid(numOfColumns)
 {
@@ -36,24 +59,61 @@ for (let outer = 0; outer < numOfColumns; ++outer)
     for (let inner = 0; inner < numOfBoxes; ++inner)
     {
         box = document.createElement('div');
-        box.classList.add('box');
+        box.classList.add('box', 'border');
         arrayOfColumns[outer].appendChild(box);
     }
 }
 
 let arrayOfBoxes = document.querySelectorAll('.box');
 
+let btnErase = document.querySelector('.btn-erase');
+btnErase.addEventListener('click', () => {
+    for (let i = 0; i < totalNumOfBoxes; ++i)
+    {
+    arrayOfBoxes[i].addEventListener('mouseover', () => {arrayOfBoxes[i].style.backgroundColor = ''});
+    }
+});
+
 for (let i = 0; i < totalNumOfBoxes; ++i)
 {
     arrayOfBoxes[i].addEventListener('mouseover', () => {arrayOfBoxes[i].style.backgroundColor = input.value});
 }
 
+input.addEventListener('click', () => {
+    for (let i = 0; i < totalNumOfBoxes; ++i)
+{
+    arrayOfBoxes[i].addEventListener('mouseover', () => {arrayOfBoxes[i].style.backgroundColor = input.value});
+}
+});
+
 
 const btnReset = document.querySelector('.btn-reset');
 btnReset.addEventListener('click', () => {
     for (let i = 0; i < totalNumOfBoxes; ++i)
-{
-    arrayOfBoxes[i].style.backgroundColor = 'white';
-}
+    {
+    arrayOfBoxes[i].style.backgroundColor = '';
+    }
 });
+
+const toggleLines = document.querySelector('.btn-toggle-lines');
+toggleLines.addEventListener('click', () => {
+    for (let i = 0; i < totalNumOfBoxes; ++i)
+    {
+    arrayOfBoxes[i].classList.toggle("border");
+    }
+})
+
+let btnRgb = document.querySelector('.btn-rgb');
+btnRgb.addEventListener('click', () => {
+    let x = randomNumber();
+    let y = randomNumber();
+    let z = randomNumber();
+
+    for (let i = 0; i < totalNumOfBoxes; ++i)
+    {
+    arrayOfBoxes[i].addEventListener('mouseover', () => {arrayOfBoxes[i].style.backgroundColor = `rgb(${x}, ${y}, ${z})`});
+    }
+})
+
 }
+
