a01c48fdfc8818d2f3032e55ab08a141fcd6b03b
scotchmint <nullptr91@tuta.io>
2022-12-20 19:52:32 +0200

    Add gray button

--- script.js
+++ script.js
@@ -1,115 +1,125 @@
-function randomNumber()
-{
-    let random = Math.floor(Math.random() * 255);
+const btnRainbow = document.querySelector(".btn-rainbow");
+const btnGray = document.querySelector(".btn-gray");
+const btnReset = document.querySelector(".btn-reset");
+const btnErase = document.querySelector(".btn-erase");
+const toggleLines = document.querySelector(".btn-toggle-lines");
+const grid = document.querySelector(".grid");
+const input = document.querySelector("#color-input");
+const slider = document.querySelector(".slider");
+const sliderOutput = document.querySelector(".show-value");
 
-    return random;
+function randomNumber() 
+{
+  let random = Math.floor(Math.random() * 360);
+  return random;
 }
 
-function resetGrid(numOfColumns)
+function resetGrid(numOfColumns) 
 {
-    for(let i = 0; i < numOfColumns; ++i)
-    {
-        grid.removeChild(grid.firstChild);
-    }
+  for (let i = 0; i < numOfColumns; ++i) 
+  {
+    grid.removeChild(grid.firstChild);
+  }
 }
-
-let grid = document.querySelector('.grid');
-let input = document.querySelector('#color-input');
 
 let numOfColumns = 16;
+
+function makeGrid(numOfColumns) 
+{
+  let numOfBoxes = numOfColumns;
+  let column;
+  let box;
+
+  // make columns based on numOfColumns
+  for (let i = 0; i < numOfColumns; ++i) {
+    column = document.createElement("div");
+    column.classList.add("column");
+    grid.appendChild(column);
+  }
+
+  let arrayOfColumns = Array.from(document.querySelectorAll(".column"));
+
+  // loop through each column
+  for (let outer = 0; outer < numOfColumns; ++outer) {
+    // add boxes in each column based on numOfBoxes
+    for (let inner = 0; inner < numOfBoxes; ++inner) {
+      box = document.createElement("div");
+      box.classList.add("box", "border");
+      arrayOfColumns[outer].appendChild(box);
+    }
+  }
+
+  // make buttons for each grid
+  // this function is too long but more efficient when the grid is resizing using the slider
+  // i have tried to split it into another function but that caused a lot of problems with slider
+  let arrayOfBoxes = Array.from(document.querySelectorAll(".box"));
+  let totalBoxes = arrayOfBoxes.length;
+
+  input.addEventListener("click", () => {
+    for (let i = 0; i < totalBoxes; ++i) {
+      arrayOfBoxes[i].addEventListener("mouseover", () => {
+        arrayOfBoxes[i].style.backgroundColor = input.value;
+      });
+    }
+  });
+
+  // default color is black
+  for (let i = 0; i < totalBoxes; ++i) {
+    arrayOfBoxes[i].addEventListener("mouseover", () => {
+      arrayOfBoxes[i].style.backgroundColor = input.value;
+    });
+  }
+
+  btnRainbow.addEventListener("click", () => {
+    for (let i = 0; i < totalBoxes; ++i) {
+      arrayOfBoxes[i].addEventListener("mouseover", () => {
+        arrayOfBoxes[
+          i
+        ].style.backgroundColor = `hsl(${randomNumber()}, 100%, 50%)`;
+      });
+    }
+  });
+
+  let grayPercent = 90;
+
+  btnGray.addEventListener("click", () => {
+    for (let i = 0; i < totalBoxes; ++i) {
+      arrayOfBoxes[i].addEventListener("mouseover", () => {
+        if (grayPercent === 0) {
+          grayPercent = 90;
+        }
+        arrayOfBoxes[i].style.backgroundColor = `hsl(0, 0%, ${--grayPercent}%)`;
+      });
+    }
+  });
+
+  btnReset.addEventListener("click", () => {
+    for (let i = 0; i < totalBoxes; ++i) {
+      arrayOfBoxes[i].style.backgroundColor = "";
+    }
+  });
+
+  btnErase.addEventListener("click", () => {
+    for (let i = 0; i < totalBoxes; ++i) {
+      arrayOfBoxes[i].addEventListener("mouseover", () => {
+        arrayOfBoxes[i].style.backgroundColor = "";
+      });
+    }
+  });
+
+  toggleLines.addEventListener("click", () => {
+    for (let i = 0; i < totalBoxes; ++i) {
+      arrayOfBoxes[i].classList.toggle("border");
+    }
+  });
+}
+
+// default grid
 makeGrid(numOfColumns);
 
-let slider = document.querySelector('.slider');
-let sliderOutput = document.querySelector('.show-value');
-
-slider.addEventListener('input', () => {
-    
-    resetGrid(numOfColumns);
-    numOfColumns = slider.value;
-    sliderOutput.textContent = `${slider.value} X ${slider.value}` ;
-    makeGrid(numOfColumns);
-    
-})
-
-function makeGrid(numOfColumns)
-{
-let numOfBoxes = numOfColumns;
-let totalNumOfBoxes = numOfColumns * numOfBoxes;
-let column;
-let box;
-
-// make columns based on numOfColumns
-for (let i = 0; i < numOfColumns; ++i)
-{
-    column = document.createElement('div');
-    column.classList.add('column');
-    grid.appendChild(column);
-}
-
-let arrayOfColumns = Array.from(document.querySelectorAll('.column'));
-
-// loop through each column
-for (let outer = 0; outer < numOfColumns; ++outer)
-{
-    // add numOfBoxes in each column
-    for (let inner = 0; inner < numOfBoxes; ++inner)
-    {
-        box = document.createElement('div');
-        box.classList.add('box', 'border');
-        arrayOfColumns[outer].appendChild(box);
-    }
-}
-
-let arrayOfBoxes = document.querySelectorAll('.box');
-
-let btnErase = document.querySelector('.btn-erase');
-btnErase.addEventListener('click', () => {
-    for (let i = 0; i < totalNumOfBoxes; ++i)
-    {
-    arrayOfBoxes[i].addEventListener('mouseover', () => {arrayOfBoxes[i].style.backgroundColor = ''});
-    }
+slider.addEventListener("input", () => {
+  resetGrid(numOfColumns);
+  numOfColumns = slider.value;
+  sliderOutput.textContent = `${slider.value} X ${slider.value}`;
+  makeGrid(numOfColumns);
 });
-
-for (let i = 0; i < totalNumOfBoxes; ++i)
-{
-    arrayOfBoxes[i].addEventListener('mouseover', () => {arrayOfBoxes[i].style.backgroundColor = input.value});
-}
-
-input.addEventListener('click', () => {
-    for (let i = 0; i < totalNumOfBoxes; ++i)
-    {
-    arrayOfBoxes[i].addEventListener('mouseover', () => {arrayOfBoxes[i].style.backgroundColor = input.value});
-    }
-});
-
-
-const btnReset = document.querySelector('.btn-reset');
-btnReset.addEventListener('click', () => {
-    for (let i = 0; i < totalNumOfBoxes; ++i)
-    {
-    arrayOfBoxes[i].style.backgroundColor = '';
-    }
-});
-
-const toggleLines = document.querySelector('.btn-toggle-lines');
-toggleLines.addEventListener('click', () => {
-    for (let i = 0; i < totalNumOfBoxes; ++i)
-    {
-    arrayOfBoxes[i].classList.toggle("border");
-    }
-})
-
-let btnRainbow = document.querySelector('.btn-rainbow');
-btnRainbow.addEventListener('click', () => {
-    for (let i = 0; i < totalNumOfBoxes; ++i)
-    {
-    arrayOfBoxes[i].addEventListener('mouseover', () => {
-        let x = randomNumber();
-        let y = randomNumber();
-        let z = randomNumber();
-        arrayOfBoxes[i].style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
-    });
-    }
-})
-
-}
